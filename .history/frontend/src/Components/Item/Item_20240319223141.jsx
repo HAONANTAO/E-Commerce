8d461cfa76d31208a56bea59c49dc39fcfdc5c4b
HAONANTAO <873190199@qq.com>
2024-03-19 22:37:39 +1100

    popular page css set up

--- .history/frontend/src/Components/Item/Item_20240319223141.jsx
+++ .history/frontend/src/Components/Item/Item_20240319223141.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Item.css";
 const Item = (props) => {
+  console.log(props);
   // 根据传入prop进行渲染
   return (
     <div className="item">
-      <img src={props.image} alt="" />
+      <img src={props.item.image} alt="" />
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">${props.new_price}</div>
-        <div className="item-price-old">${props.old_price}</div>
+        <div className="item-price-new">{props.new_price}</div>
+        <div className="item-price-old">{props.old_price}</div>
       </div>
     </div>
   );
